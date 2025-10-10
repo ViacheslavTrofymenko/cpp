821e2bd9c7e1deea94d14005d79b5a15551560d5
ViacheslavTrofymenko <trofymenko.v.n@gmail.com>
2025-10-10 19:09:18 +0200

    Fixed in Human B seg error when wheapon is not set

--- cpp01/ex03/src/HumanB.cpp
+++ cpp01/ex03/src/HumanB.cpp
@@ -6,18 +6,25 @@
 /*   By: vtrofyme <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/09 00:55:13 by vtrofyme          #+#    #+#             */
-/*   Updated: 2025/10/09 01:00:08 by vtrofyme         ###   ########.fr       */
+/*   Updated: 2025/10/10 19:08:12 by vtrofyme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "HumanB.hpp"
 
 HumanB::HumanB(std::string name) : name(name), weapon(NULL) {};
+
 HumanB::~HumanB() {};
 
 void HumanB::attack( void )
 {
-	std::cout << this->name << " attacks with their " << this->weapon->getType() << std::endl;
+	if (weapon == NULL)
+	{
+		std::cout << "My name is " << this->name << " and i don't have weapon."
+		" I cannot attack" << std::endl;
+	}
+	else
+		std::cout << this->name << " attacks with their " << this->weapon->getType() << std::endl;
 }
 
 void	HumanB::setWeapon(Weapon &weapon)
