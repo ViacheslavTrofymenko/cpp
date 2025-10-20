cca9e579e492f15231a4f72e72de84ebcbd235fc
Viacheslav Trofymenko <vtrofyme@c2r2s1.42madrid.com>
2025-10-20 14:51:53 +0200

    wip

--- cpp03/ex02/include/FragTrap.hpp
+++ cpp03/ex02/include/FragTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: vtrofyme <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/20 14:20:43 by vtrofyme          #+#    #+#             */
-/*   Updated: 2025/10/20 14:22:26 by vtrofyme         ###   ########.fr       */
+/*   Updated: 2025/10/20 14:28:08 by vtrofyme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,13 @@
 #include "ClapTrap.hpp"
 class FragTrap: public ClapTrap
 {
-
-}
+	public:
+		FragTrap();
+		FragTrap(std::string name);
+		FragTrap( const FragTrap& copy);
+		FragTrap& operator = (const FragTrap& other);
+		~FragTrap();
+		void highFivesGuys(void);
+};
 
 #endif
