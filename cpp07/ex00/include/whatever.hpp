85f57327ce31eec801bdb30f8150c29c7c5b5634
ViacheslavTrofymenko <trofymenko.v.n@gmail.com>
2025-11-12 18:48:49 +0100

    Added ex00 to cpp07

---
+++ cpp07/ex00/include/whatever.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vtrofyme <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/12 17:07:46 by vtrofyme          #+#    #+#             */
+/*   Updated: 2025/11/12 17:09:57 by vtrofyme         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+template <typename T>
+void swap(T &a, T &b) {
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
+
+template <typename T>
+T const &min(T const &a, T const &b)
+{
+	if (a < b)
+		return (a);
+    return (b);
+}
+
+template <typename T>
+T const &max(T const &a, T const &b)
+{
+	if (a > b)
+		return (a);
+	return (b);
+}
+
+#endif
