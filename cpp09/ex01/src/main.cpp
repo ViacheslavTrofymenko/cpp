be770ae8ed8d657cc73b4ddd9c39a6dd70a778ed
Viacheslav Trofymenko <vtrofyme@c2r2s6.42madrid.com>
2025-11-26 13:51:02 +0100

    Fixed ex01 in cpp09

--- cpp09/ex01/src/main.cpp
+++ cpp09/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: vtrofyme <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/25 16:50:00 by vtrofyme          #+#    #+#             */
-/*   Updated: 2025/11/25 20:43:04 by vtrofyme         ###   ########.fr       */
+/*   Updated: 2025/11/26 13:43:06 by vtrofyme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,25 @@
 #include <stdexcept>
 #include <cstdlib>
 
-int main(int argc, char **argv) {
-    if (argc != 2) {
-        std::cerr << "Usage: ./RPN \"<RPN expression>\"" << std::endl;
-        return 1;
-    }
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cerr << "Usage: ./RPN \"<RPN expression>\"" << std::endl;
+		return 1;
+	}
 
-    try {
-        RPN calculator;
-        int result = calculator.calculate(argv[1]);
-        std::cout << result << std::endl;
-    } catch (const std::runtime_error& e) {
-        std::cerr << e.what() << std::endl;
-        return 1;
-    }
+	try
+	{
+		RPN calculator;
+		int result = calculator.calculate(argv[1]);
+		std::cout << result << std::endl;
+	}
+	catch (const std::runtime_error &e)
+	{
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
 
-    return 0;
+	return (0);
 }
